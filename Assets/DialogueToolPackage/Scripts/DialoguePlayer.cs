4d247349d09eac198c925d63c038c370b7a49d5c
Chris <cmagnuson0489@gmail.com>
2020-01-02 16:23:25 -0800

    Initial Project Setup

---
+++ Assets/DialogueToolPackage/Scripts/DialoguePlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialoguePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartDialogue()
+    {
+
+    }
+
+    public void PauseDialogue()
+    {
+
+    }
+
+    public void StopDialogue()
+    {
+
+    }
+}
---
+++ Assets/DialogueToolPackage/Scripts/DialogueTree.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DialogueTreeAsset", menuName = "ScriptableObjects/DialogueTreeScriptableObject", order = 1)]
+public class DialogueTree : ScriptableObject
+{
+    [TextArea(10, 15)]
+    public List<string> dialogueTreeElements = new List<string>();
+
+    public DialogueTree nextDialogueTree;
+}
