6a7e9584a561af9e443dc2c18d23f1ad43bc420d
Ryan Zagala <gman10501@gmail.com>
2020-01-15 17:00:07 -0800

    Started unit testing DialogueTrigger class functions.

---
+++ Assets/Editor/Tests/DialogueTriggerTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEditor;
+
+namespace Tests
+{
+    public class DialogueTriggerTest
+    {
+        public DialogueTree dialogueTree;
+
+        [Test]
+        public void TriggerDialogueTest()
+        {
+            if (DialogueManager.instance)
+            {
+                Assert.IsNotNull(DialogueManager.instance);
+                DialogueManager.instance.StartDialogue(dialogueTree);
+            }
+            else
+            {
+                Assert.IsNull(DialogueManager.instance);
+                Debug.LogError("DialogueManager instance is not set! Please place DialogueManager in the scene.");
+                LogAssert.Expect(LogType.Error, "DialogueManager instance is not set! Please place DialogueManager in the scene.");
+            }
+        }
+    }
+}
